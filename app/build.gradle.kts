00067136d9e1b95d15c91a18117b5527b1eba230
MReP1 <2351267654@qq.com>
2023-01-18 01:12:06 +0800

    feat: optimizing libs script.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,38 +23,34 @@ android {
 
 dependencies {
 
-    implementation(Dependencies.Androidx.core_ktx)
-    implementation(Dependencies.Androidx.appcompat)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
 
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation(libs.google.android.material)
+    implementation(libs.androidx.constraintLayout)
+    implementation(libs.androidx.recyclerView)
 
-    implementation("androidx.fragment:fragment-ktx:1.4.1")
-    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.fragment.ktx)
 
-    //SplashScreen
-    implementation("androidx.core:core-splashscreen:1.0.0")
+    // SplashScreen
+    implementation(libs.androidx.core.splashscreen)
 
-    val room_version = "2.4.2"
-    //Room数据库
-    implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
+    // DataStore
+    implementation(libs.androidx.dataStore.preferences)
 
-    //协程
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    // Room database
+    implementation(libs.room.runtime)
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
 
-    val lifecycle_version = "2.4.1"
+    // coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
 
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")//lifecycleScope
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")//viewModelScope
-    implementation("androidx.lifecycle:lifecycle-process:$lifecycle_version")
-
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
+    // lifecycle
+    implementation(libs.androidx.lifecycle.rumtime.ktx)
+    implementation(libs.androidx.lifecycle.viewModel.ktx)
 
     //MPAndroidChart
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
@@ -63,8 +59,5 @@ dependencies {
     implementation(project(":RichText"))
     //自定义Calendar
     implementation(project(":calendarview"))
-
-    //DataStore
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
 }
