7093c873ddfcbdf5a7a9b4e594a0223dbf83352a
fne <2351267654@qq.com>
2023-02-05 03:30:46 +0800

    feat: update fragment ktx version.

--- app/src/main/java/little/goose/account/ui/schedule/ScheduleViewModel.kt
+++ app/src/main/java/little/goose/account/ui/schedule/ScheduleViewModel.kt
@@ -2,14 +2,17 @@ package little.goose.account.ui.schedule
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import little.goose.account.common.receiver.DeleteItemBroadcastReceiver
 import little.goose.account.common.receiver.NormalBroadcastReceiver
 import little.goose.account.logic.ScheduleRepository
 import little.goose.account.logic.data.entities.Schedule
+import javax.inject.Inject
 
-class ScheduleViewModel : ViewModel() {
+@HiltViewModel
+class ScheduleViewModel @Inject constructor() : ViewModel() {
 
     private val _schedules = MutableStateFlow<List<Schedule>>(listOf())
     val schedules = _schedules.asStateFlow()
