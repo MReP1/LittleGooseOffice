2a7cb31e4ad165dd94c72536dc8620de940207a6
Fne <2351267654@qq.com>
2023-04-01 23:38:07 +0800

    fix: InputDialog add ImePadding

--- core/design-system/src/main/java/little/goose/design/system/component/dialog/InputDialog.kt
+++ core/design-system/src/main/java/little/goose/design/system/component/dialog/InputDialog.kt
@@ -1,10 +1,7 @@
 package little.goose.design.system.component.dialog
 
 import android.view.ViewTreeObserver.OnWindowFocusChangeListener
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -45,7 +42,9 @@ fun InputDialog(
             onDispose { softwareKeyboardController = null }
         }
         InputDialogScreen(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .imePadding(),
             text = text,
             onConfirm = {
                 onConfirm(it)
