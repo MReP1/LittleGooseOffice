9bf71e96eab63d80e89e3162d87d2f449b98d0dc
Fne <2351267654@qq.com>
2023-05-07 04:07:48 +0800

    feat: Note数据库更新

--- feature/note/src/main/java/little/goose/note/ui/note/NoteViewModel.kt
+++ feature/note/src/main/java/little/goose/note/ui/note/NoteViewModel.kt
@@ -4,14 +4,13 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.drop
+import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
-import little.goose.common.constants.TYPE_ADD
-import little.goose.common.constants.TYPE_MODIFY
 import little.goose.note.data.constants.KEY_NOTE
 import little.goose.note.data.entities.Note
 import little.goose.note.logic.NoteRepository
@@ -23,11 +22,15 @@ class NoteViewModel @Inject constructor(
     private val noteRepository: NoteRepository
 ) : ViewModel() {
 
+    private enum class Type {
+        ADD, MODIFY
+    }
+
     val note: StateFlow<Note> = savedStateHandle.getStateFlow(KEY_NOTE, Note())
 
-    var type = if (note.value.id == null) TYPE_ADD else TYPE_MODIFY
+    private val type get() = if (note.value.id == null) Type.ADD else Type.MODIFY
 
-    private fun isNoteBlank() = note.value.title.isBlank() && note.value.content.isBlank()
+    private val isNoteBlank get() = note.value.title.isBlank() && note.value.content.isBlank()
 
     val noteScreenState = note.map {
         NoteScreenState(note = it, onTitleChange = ::changeTitle, onContentChange = ::changeContent)
@@ -45,25 +48,27 @@ class NoteViewModel @Inject constructor(
         savedStateHandle[KEY_NOTE] = note.value.copy(content = content)
     }
 
-    fun updateDatabase() {
-        viewModelScope.launch {
-            if (!isNoteBlank()) {
-                noteRepository.updateNote(note.value)
-            } else {
-                noteRepository.deleteNote(note.value)
+    init {
+        note.drop(1).onEach {
+            when (type) {
+                Type.ADD -> insertDatabase()
+                Type.MODIFY -> updateDatabase()
             }
+        }.launchIn(viewModelScope)
+    }
+
+    private suspend fun updateDatabase() {
+        if (!isNoteBlank) {
+            noteRepository.updateNote(note.value)
+        } else {
+            noteRepository.deleteNote(note.value)
         }
     }
 
-    fun insertDatabase() {
-        if (!isNoteBlank()) {
-            viewModelScope.launch {
-                val id = noteRepository.addNote(note.value)
-                if (isActive) {
-                    savedStateHandle[KEY_NOTE] = note.value.copy(id = id)
-                    type = TYPE_MODIFY
-                }
-            }
+    private suspend fun insertDatabase() {
+        if (!isNoteBlank) {
+            val id = noteRepository.addNote(note.value)
+            savedStateHandle[KEY_NOTE] = note.value.copy(id = id)
         }
     }
 
