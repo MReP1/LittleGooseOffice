3faf5b53f8a772cd0e9461eaaeac02688839cbd5
Fne <2351267654@qq.com>
2023-05-23 16:22:55 +0800

    feat: 手动加载动画

--- core/ui/src/main/java/little/goose/ui/screen/LoadingScreen.kt
+++ core/ui/src/main/java/little/goose/ui/screen/LoadingScreen.kt
@@ -1,18 +1,56 @@
 package little.goose.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.delay
 import little.goose.design.system.theme.AccountTheme
+import little.goose.ui.R
 
 @Composable
-fun LoadingScreen(
+fun LittleGooseLoadingScreen(
+    modifier: Modifier = Modifier
+) {
+    LoadingScreen(
+        modifier = modifier,
+        content = {
+            val normalGoose = painterResource(id = R.drawable.ic_little_goose)
+            val backGoose = painterResource(id = R.drawable.ic_little_goose_back)
+            val foreGoose = painterResource(id = R.drawable.ic_little_goose_fore)
+            var painter by remember { mutableStateOf(normalGoose) }
+            Image(painter = painter, contentDescription = "Loading")
+            LaunchedEffect(Unit) {
+                delay(80)
+                while (true) {
+                    painter = backGoose
+                    delay(100)
+                    painter = normalGoose
+                    delay(80)
+                    painter = foreGoose
+                    delay(100)
+                    painter = normalGoose
+                    delay(80)
+                }
+            }
+        }
+    )
+}
+
+@Composable
+internal fun LoadingScreen(
     modifier: Modifier = Modifier,
     backgroundColor: Color = MaterialTheme.colorScheme.background,
     content: @Composable () -> Unit = {}
