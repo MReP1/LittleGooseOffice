7779a02dc352935212ffcc371f4d50f428c86c50
Fne <2351267654@qq.com>
2023-05-24 17:06:17 +0800

    feat: update version code.

--- build-logic/convention/src/main/java/AndroidConfigConventions.kt
+++ build-logic/convention/src/main/java/AndroidConfigConventions.kt
@@ -14,8 +14,8 @@ object AndroidConfigConventions {
         private const val PACKAGE_NAME = "little.goose.account"
         const val APPLICATION_ID = PACKAGE_NAME
 
-        const val VERSION_CODE = 18
-        const val VERSION_NAME = "2.0.1"
+        const val VERSION_CODE = 19
+        const val VERSION_NAME = "2.0.2"
     }
 
 }
