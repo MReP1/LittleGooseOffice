21ba407f48f7c1addbf5dfd8f45628063c0771ec
Fne <2351267654@qq.com>
2023-05-24 15:48:59 +0800

    feat: keep class searchType

--- feature/search/src/main/java/little/goose/search/SearchType.kt
+++ feature/search/src/main/java/little/goose/search/SearchType.kt
@@ -1,8 +1,10 @@
 package little.goose.search
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
+@Keep
 @Parcelize
 enum class SearchType : Parcelable {
     Transaction, Note, Memorial, Schedule;
