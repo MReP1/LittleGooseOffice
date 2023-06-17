c27a7bc58554d14695fe081db44db62b53fedf79
Fne <2351267654@qq.com>
2023-06-17 13:04:24 +0800

    feat: update aversion code.

--- build-logic/convention/src/main/java/AndroidConfigConventions.kt
+++ build-logic/convention/src/main/java/AndroidConfigConventions.kt
@@ -14,8 +14,8 @@ object AndroidConfigConventions {
         private const val PACKAGE_NAME = "little.goose.account"
         const val APPLICATION_ID = PACKAGE_NAME
 
-        const val VERSION_CODE = 22
-        const val VERSION_NAME = "2.0.5"
+        const val VERSION_CODE = 23
+        const val VERSION_NAME = "2.0.6"
     }
 
 }
