ede3871990ea76d05ab307738d4283121c943802
Fne <2351267654@qq.com>
2023-06-17 13:10:36 +0800

    ui: change cancel button color in DialogButtonGroup

--- core/design-system/src/main/java/little/goose/design/system/component/dialog/DialogButtonGroup.kt
+++ core/design-system/src/main/java/little/goose/design/system/component/dialog/DialogButtonGroup.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
@@ -29,6 +31,9 @@ fun DialogButtonGroup(
                 .weight(1F)
                 .fillMaxHeight(),
             shape = RectangleShape,
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary
+            )
         ) {
             startButtonContent()
         }
