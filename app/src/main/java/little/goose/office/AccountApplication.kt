0b1a8df8d4243b5255bfb0e735767dc46123785b
Fne <2351267654@qq.com>
2023-06-18 17:20:18 +0800

    feat: 优化DataStore代码

--- app/src/main/java/little/goose/office/AccountApplication.kt
+++ app/src/main/java/little/goose/office/AccountApplication.kt
@@ -3,10 +3,8 @@ package little.goose.office
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import little.goose.common.di.AppCoroutineScope
-import little.goose.home.logic.HomePageDataHolder
 import javax.inject.Inject
 
 @HiltAndroidApp
@@ -16,9 +14,6 @@ class AccountApplication : Application() {
     @AppCoroutineScope
     lateinit var appScope: CoroutineScope
 
-    @Inject
-    lateinit var holder: HomePageDataHolder
-
     override fun onCreate() {
         super.onCreate()
         initData()
@@ -27,7 +22,6 @@ class AccountApplication : Application() {
     //初始化数据 防止第一次打开还要加载
     private fun initData() {
         appScope.launch {
-            holder.homePage.first { it != -1 }
             isAppInit = true
         }
     }
--- app/src/main/java/little/goose/office/MainViewModel.kt
+++ app/src/main/java/little/goose/office/MainViewModel.kt
@@ -4,17 +4,27 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
+import little.goose.home.logic.HomePageDataHolder
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    appDataHolder: AppDataHolder
+    appDataHolder: AppDataHolder,
+    homePageDataHolder: HomePageDataHolder
 ) : ViewModel() {
 
-    val appState = appDataHolder.designSystemDataHolder.themeConfig.map {
-        AppState.Success(it)
+    val appState = combine(
+        appDataHolder.designSystemDataHolder.themeConfig,
+        homePageDataHolder.homePage
+    ) { themeConfig, homePage ->
+        // 预加载 HomePage
+        if (homePage != -1) {
+            AppState.Success(themeConfig)
+        } else {
+            AppState.Loading
+        }
     }.stateIn(viewModelScope, SharingStarted.Eagerly, AppState.Loading)
 
 }
