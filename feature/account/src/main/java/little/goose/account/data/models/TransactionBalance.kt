06a88d12c2454dbc25cccd52aed25210d407493f
fne <2351267654@qq.com>
2023-12-10 17:10:47 +0800

    opt: use stable define TransactionBalance

--- feature/account/src/main/java/little/goose/account/data/models/TransactionBalance.kt
+++ feature/account/src/main/java/little/goose/account/data/models/TransactionBalance.kt
@@ -1,8 +1,10 @@
 package little.goose.account.data.models
 
+import androidx.compose.runtime.Stable
 import java.math.BigDecimal
-import java.util.*
+import java.util.Date
 
+@Stable
 data class TransactionBalance(
     val time: Date = Date(),
     var expense: BigDecimal = BigDecimal(0),
