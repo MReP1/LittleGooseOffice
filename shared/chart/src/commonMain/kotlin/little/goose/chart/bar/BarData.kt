0f9ccd8d65dceae3a9bcf66b0367586fca961268
fne <2351267654@qq.com>
2023-12-10 16:58:15 +0800

    fix: savable crash

--- shared/chart/src/commonMain/kotlin/little/goose/chart/bar/BarData.kt
+++ shared/chart/src/commonMain/kotlin/little/goose/chart/bar/BarData.kt
@@ -2,6 +2,7 @@ package little.goose.chart.bar
 
 import androidx.compose.runtime.saveable.listSaver
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 
 data class BarData(
     val xText: String,
@@ -13,12 +14,17 @@ data class BarData(
         val saver = listSaver<BarData?, Any?>(
             save = { barData ->
                 barData?.let {
-                    listOf(it.xText, it.amount, it.color, it.id)
+                    listOf(it.xText, it.amount, it.color.toArgb(), it.id)
                 } ?: emptyList()
             },
             restore = {
                 if (it.isEmpty()) null else {
-                    BarData(it[0] as String, it[1] as Float, it[2] as Color, it[3] as String)
+                    BarData(
+                        it[0] as String,
+                        it[1] as Float,
+                        Color(color = it[2] as Int),
+                        it[3] as String
+                    )
                 }
             }
         )
