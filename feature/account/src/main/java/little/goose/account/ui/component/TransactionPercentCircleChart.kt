bd9bb648a3531db34eea0b797f5940a78289fcc6
fne <2351267654@qq.com>
2023-12-17 02:49:27 +0800

    fix: show circle chart label space in last index.

--- feature/account/src/main/java/little/goose/account/ui/component/TransactionPercentCircleChart.kt
+++ feature/account/src/main/java/little/goose/account/ui/component/TransactionPercentCircleChart.kt
@@ -1,11 +1,24 @@
 package little.goose.account.ui.component
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -82,9 +95,7 @@ fun TransactionPercentCircleChart(
             for (index in dataList.indices) {
                 val data = dataList[index]
                 ChartLabel(color = data.color, text = data.content)
-                if (index < dataList.lastIndex) {
-                    Spacer(modifier = Modifier.width(6.dp))
-                }
+                Spacer(modifier = Modifier.width(6.dp))
             }
         }
     }
