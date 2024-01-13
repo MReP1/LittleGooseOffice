e61b6c6bfd3b83c4b9ed65817c624452f1776a63
fne <2351267654@qq.com>
2024-01-14 01:14:55 +0800

    optimize: AutoResizedText.

--- core/design-system/src/main/java/little/goose/design/system/component/AutoResizedText.kt
+++ core/design-system/src/main/java/little/goose/design/system/component/AutoResizedText.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,9 +15,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.graphics.drawscope.ContentDrawScope
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.node.DrawModifierNode
+import androidx.compose.ui.node.LayoutModifierNode
+import androidx.compose.ui.node.ModifierNodeElement
+import androidx.compose.ui.node.invalidateDraw
+import androidx.compose.ui.node.invalidateMeasurement
+import androidx.compose.ui.platform.InspectorInfo
 import androidx.compose.ui.text.TextLayoutResult
 import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.TextStyle
@@ -26,9 +37,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.isUnspecified
+import androidx.compose.ui.unit.takeOrElse
 import androidx.compose.ui.unit.toOffset
 import little.goose.design.system.theme.GooseTheme
+import kotlin.math.max
 
 @Composable
 fun AutoResizedText(
@@ -41,44 +53,189 @@ fun AutoResizedText(
     textAlignment: Alignment = Alignment.CenterStart
 ) {
     require(factor < 1F && factor > 0F) { "Scale factor must me in 0..1" }
-    val defaultFontSize = MaterialTheme.typography.bodyMedium.fontSize
-    val layoutDirection = LocalLayoutDirection.current
+    val fontSize = style.fontSize.takeOrElse { MaterialTheme.typography.bodyMedium.fontSize }
+    val textColor = color.takeOrElse { LocalContentColor.current }
     Spacer(
         modifier = modifier
+            .resizableText(
+                textMeasurer = textMeasurer,
+                text = text,
+                textStyle = style.merge(color = textColor, fontSize = fontSize),
+                alignment = textAlignment,
+                factor = factor
+            )
             .fillMaxSize()
-            .drawWithCache {
-                var fontSize = if (style.fontSize.isUnspecified) defaultFontSize else style.fontSize
-                var textLayoutResult: TextLayoutResult
-                var isFirstMeasure = true
-                do {
-                    if (!isFirstMeasure) {
-                        fontSize *= factor
-                    }
-                    textLayoutResult = textMeasurer.measure(
-                        text = text,
-                        style = style.copy(fontSize = fontSize),
-                        maxLines = 1,
-                        softWrap = false,
-                        constraints = Constraints(maxWidth = size.width.toInt())
-                    )
-                    isFirstMeasure = false
-                } while (textLayoutResult.didOverflowWidth)
-
-                val offset = textAlignment.align(
-                    textLayoutResult.size,
-                    IntSize(size.width.toInt(), size.height.toInt()),
-                    layoutDirection
-                ).toOffset()
-
-                onDrawWithContent {
-                    drawText(
-                        textLayoutResult = textLayoutResult,
-                        color = color,
-                        topLeft = offset
-                    )
-                }
-            }
     )
+}
+
+private fun Modifier.resizableText(
+    textMeasurer: TextMeasurer,
+    textStyle: TextStyle,
+    text: String,
+    factor: Float,
+    alignment: Alignment
+): Modifier = this then ResizableTextModifierElement(
+    textMeasurer, textStyle, text, factor, alignment
+)
+
+private class ResizableTextNode(
+    private var textMeasurer: TextMeasurer,
+    private var textStyle: TextStyle,
+    private var text: String,
+    private var factor: Float,
+    private var alignment: Alignment
+) : Modifier.Node(), LayoutModifierNode, DrawModifierNode {
+
+    private var textLayoutResult: TextLayoutResult? = null
+    private var topLeftOffset = Offset(0F, 0F)
+    private var layoutChanged = false
+    private var textChanged = false
+    private var lastMaxWidth = 0
+
+    fun updateTextMeasure(textMeasurer: TextMeasurer) {
+        this.textMeasurer = textMeasurer
+    }
+
+    fun updateDraw(textStyle: TextStyle): Boolean {
+        return !textStyle.hasSameDrawAffectingAttributes(this.textStyle)
+    }
+
+    fun updateText(text: String): Boolean {
+        if (this.text != text) {
+            this.text = text
+            return true
+        }
+        return false
+    }
+
+    fun updateLayout(textStyle: TextStyle, alignment: Alignment, factor: Float): Boolean {
+        var changed: Boolean
+        changed = !textStyle.hasSameLayoutAffectingAttributes(this.textStyle)
+        this.textStyle = textStyle
+        if (this.alignment != alignment) {
+            this.alignment = alignment
+            changed = true
+        }
+        if (this.factor != factor) {
+            this.factor = factor
+            changed = true
+        }
+        return changed
+    }
+
+    fun doInvalidations(
+        layoutChanged: Boolean,
+        textChanged: Boolean,
+        drawChanged: Boolean
+    ) {
+        if (!isAttached) {
+            return
+        }
+        this.layoutChanged = layoutChanged
+        this.textChanged = textChanged
+        if (layoutChanged || textChanged) {
+            invalidateMeasurement()
+            invalidateDraw()
+        }
+        if (drawChanged) {
+            invalidateDraw()
+        }
+    }
+
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+        if (textLayoutResult == null
+            || layoutChanged
+            || textChanged
+            || lastMaxWidth != constraints.maxWidth
+        ) {
+            textChanged = false
+            layoutChanged = false
+            lastMaxWidth = constraints.maxWidth
+
+            var isFirstMeasure = true
+            do {
+                if (!isFirstMeasure) {
+                    textStyle = textStyle.copy(fontSize = textStyle.fontSize * factor)
+                }
+                textLayoutResult = textMeasurer.measure(
+                    text = text,
+                    style = textStyle,
+                    maxLines = 1,
+                    softWrap = false,
+                    constraints = constraints.copy(maxHeight = Int.MAX_VALUE)
+                )
+                isFirstMeasure = false
+            } while (textLayoutResult?.didOverflowWidth == true)
+        }
+        topLeftOffset = alignment.align(
+            textLayoutResult!!.size,
+            IntSize(constraints.maxWidth, constraints.maxHeight),
+            layoutDirection
+        ).toOffset()
+        val placeable = measurable.measure(constraints)
+        return layout(
+            max(textLayoutResult?.size?.width ?: 0, constraints.maxWidth),
+            max(textLayoutResult?.size?.height ?: 0, constraints.maxHeight)
+        ) {
+            placeable.place(0, 0)
+        }
+    }
+
+    override fun ContentDrawScope.draw() {
+        textLayoutResult?.let {
+            drawText(it, topLeft = topLeftOffset, color = textStyle.color)
+        }
+    }
+
+}
+
+private class ResizableTextModifierElement(
+    private val textMeasurer: TextMeasurer,
+    private val textStyle: TextStyle,
+    private val text: String,
+    private val factor: Float,
+    private val alignment: Alignment
+) : ModifierNodeElement<ResizableTextNode>() {
+
+    override fun create(): ResizableTextNode {
+        return ResizableTextNode(textMeasurer, textStyle, text, factor, alignment)
+    }
+
+    override fun update(node: ResizableTextNode) {
+        node.updateTextMeasure(textMeasurer)
+        node.doInvalidations(
+            drawChanged = node.updateDraw(textStyle),
+            layoutChanged = node.updateLayout(textStyle, alignment, factor),
+            textChanged = node.updateText(text)
+        )
+    }
+
+    @Suppress("RedundantIf")
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ResizableTextModifierElement) return false
+        if (textStyle != other.textStyle) return false
+        if (text != other.text) return false
+        if (factor != other.factor) return false
+        if (alignment != other.alignment) return false
+        // TextMeasurer no need to update frequently, so we don't need to equals TextMeasurer.
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return textStyle.hashCode() +
+                text.hashCode() +
+                factor.hashCode() +
+                alignment.hashCode()
+    }
+
+    override fun InspectorInfo.inspectableProperties() {
+        // Show nothing in the inspector.
+    }
+
 }
 
 @Preview(showBackground = true)
