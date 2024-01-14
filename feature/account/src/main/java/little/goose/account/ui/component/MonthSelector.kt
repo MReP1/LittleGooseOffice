b06f36ae70f976a3148d72d9380692330a365dc6
fne <2351267654@qq.com>
2024-01-14 17:06:13 +0800

    optimize: MonthSelector.

--- feature/account/src/main/java/little/goose/account/ui/component/MonthSelector.kt
+++ feature/account/src/main/java/little/goose/account/ui/component/MonthSelector.kt
@@ -1,11 +1,17 @@
 package little.goose.account.ui.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,8 +19,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import little.goose.account.R
+import little.goose.design.system.theme.GooseTheme
 
 data class MonthSelectorState(
     val year: Int,
@@ -29,59 +37,75 @@ fun MonthSelector(
     state: MonthSelectorState,
     onSelectTimeClick: () -> Unit
 ) {
-    Row(modifier = modifier.clip(shape)) {
-        Surface(
+    Row(
+        modifier = modifier
+            .clip(shape)
+            .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp))
+    ) {
+        Box(
             modifier = Modifier
                 .weight(1F)
-                .fillMaxHeight(),
-            onClick = {
-                val year = if (state.month == 1) state.year - 1 else state.year
-                val month = if (state.month == 1) 12 else state.month - 1
-                state.onTimeChange(year, month)
-            },
-            tonalElevation = 6.dp
+                .fillMaxHeight()
+                .clip(MaterialTheme.shapes.large)
+                .clickable {
+                    val year = if (state.month == 1) state.year - 1 else state.year
+                    val month = if (state.month == 1) 12 else state.month - 1
+                    state.onTimeChange(year, month)
+                },
+            contentAlignment = Alignment.Center
         ) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(
-                    text = if (state.month == 1) {
-                        "12"
-                    } else {
-                        "${state.month - 1}"
-                    } + stringResource(id = R.string.month)
-                )
-            }
+            Text(
+                text = if (state.month == 1) {
+                    "12"
+                } else {
+                    "${state.month - 1}"
+                } + stringResource(id = R.string.month)
+            )
         }
         Surface(
             modifier = Modifier
                 .weight(1F)
                 .fillMaxHeight(),
             onClick = onSelectTimeClick,
-            tonalElevation = 2.dp
+            color = MaterialTheme.colorScheme.secondaryContainer,
+            shape = MaterialTheme.shapes.large
         ) {
             Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 Text(text = "${state.month}" + stringResource(id = R.string.month))
             }
         }
-        Surface(
+        Box(
             modifier = Modifier
                 .weight(1F)
-                .fillMaxHeight(),
-            onClick = {
-                val year = if (state.month == 12) state.year + 1 else state.year
-                val month = if (state.month == 12) 1 else state.month + 1
-                state.onTimeChange(year, month)
-            },
-            tonalElevation = 6.dp
+                .fillMaxHeight()
+                .clip(MaterialTheme.shapes.large)
+                .clickable {
+                    val year = if (state.month == 12) state.year + 1 else state.year
+                    val month = if (state.month == 12) 1 else state.month + 1
+                    state.onTimeChange(year, month)
+                },
+            contentAlignment = Alignment.Center
         ) {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Text(
-                    text = if (state.month == 12) {
-                        "1"
-                    } else {
-                        "${state.month + 1}"
-                    } + stringResource(id = R.string.month)
-                )
-            }
+            Text(
+                text = if (state.month == 12) {
+                    "1"
+                } else {
+                    "${state.month + 1}"
+                } + stringResource(id = R.string.month)
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun PreviewMonthSelector() = GooseTheme {
+    MonthSelector(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(48.dp),
+        shape = RectangleShape,
+        state = MonthSelectorState(2024, 12, onTimeChange = { _, _ -> }),
+        onSelectTimeClick = {}
+    )
 }
