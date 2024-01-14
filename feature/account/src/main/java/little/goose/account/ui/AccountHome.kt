db6d2cd5e79e5364dcd629fbd873709af6a44280
fne <2351267654@qq.com>
2024-01-14 23:28:39 +0800

    Account Home adapt landscape

--- feature/account/src/main/java/little/goose/account/ui/AccountHome.kt
+++ feature/account/src/main/java/little/goose/account/ui/AccountHome.kt
@@ -1,19 +1,24 @@
 package little.goose.account.ui
 
 import android.icu.util.Calendar
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.windowsizeclass.WindowHeightSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import little.goose.account.ui.component.AccountTitle
 import little.goose.account.ui.component.AccountTitleState
@@ -31,6 +36,8 @@ import little.goose.common.utils.setYear
 import little.goose.design.system.component.dialog.TimeSelectorCenterDialog
 import little.goose.design.system.component.dialog.rememberDialogState
 import little.goose.design.system.theme.GooseTheme
+import little.goose.design.system.theme.LocalWindowSizeClass
+import little.goose.design.system.util.PreviewMultipleScreenSizes
 import little.goose.ui.icon.PullToSearchIcon
 import little.goose.ui.surface.PullSurface
 
@@ -44,6 +51,8 @@ fun AccountHome(
     onNavigateToSearch: () -> Unit,
     onNavigateToAccountAnalysis: () -> Unit
 ) {
+    val selectorTimeDialogState = rememberDialogState()
+
     PullSurface(
         modifier = modifier,
         onPull = onNavigateToSearch,
@@ -60,55 +69,90 @@ fun AccountHome(
             )
         },
         content = {
-            TransactionColumn(
-                modifier = Modifier.fillMaxSize(),
-                state = transactionColumnState,
-                title = {
-                    AccountTitle(
+            val windowSizeClass = LocalWindowSizeClass.current
+            val isLandscape = windowSizeClass.heightSizeClass == WindowHeightSizeClass.Compact
+            Row {
+                if (isLandscape) {
+                    Column(
                         modifier = Modifier
-                            .fillMaxWidth()
-                            .height(136.dp),
-                        accountTitleState = accountTitleState,
-                        onNavigateToAnalysis = onNavigateToAccountAnalysis
-                    )
-                },
-                monthSelector = {
-                    val selectorTimeDialogState = rememberDialogState()
-
-                    MonthSelector(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(42.dp),
-                        state = monthSelectorState,
-                        shape = MaterialTheme.shapes.large,
-                        onSelectTimeClick = { selectorTimeDialogState.show() }
-                    )
-
-                    TimeSelectorCenterDialog(
-                        state = selectorTimeDialogState,
-                        initTime = remember(monthSelectorState.year, monthSelectorState.month) {
-                            calendar.apply {
-                                clear()
-                                setYear(monthSelectorState.year)
-                                setMonth(monthSelectorState.month)
-                            }.time
-                        },
-                        type = TimeType.YEAR_MONTH,
-                        onConfirm = {
-                            val cal = calendar.apply { time = it }
-                            monthSelectorState.onTimeChange(cal.getYear(), cal.getMonth())
-                        }
-                    )
-                },
-                onTransactionEdit = { transaction ->
-                    transaction.id?.run(onNavigateToTransactionScreen)
+                            .fillMaxHeight()
+                            .width(320.dp)
+                    ) {
+                        AccountTitle(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(136.dp),
+                            accountTitleState = accountTitleState,
+                            onNavigateToAnalysis = onNavigateToAccountAnalysis
+                        )
+                        Spacer(modifier = Modifier.height(12.dp))
+                        MonthSelector(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(42.dp),
+                            state = monthSelectorState,
+                            shape = MaterialTheme.shapes.large,
+                            onSelectTimeClick = { selectorTimeDialogState.show() }
+                        )
+                    }
                 }
-            )
+                TransactionColumn(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1F),
+                    state = transactionColumnState,
+                    title = if (!isLandscape) {
+                        {
+                            AccountTitle(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(136.dp),
+                                accountTitleState = accountTitleState,
+                                onNavigateToAnalysis = onNavigateToAccountAnalysis
+                            )
+                        }
+                    } else null,
+                    monthSelector = if (!isLandscape) {
+                        {
+                            MonthSelector(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(42.dp),
+                                state = monthSelectorState,
+                                shape = MaterialTheme.shapes.large,
+                                onSelectTimeClick = { selectorTimeDialogState.show() }
+                            )
+                        }
+                    } else null,
+                    onTransactionEdit = { transaction ->
+                        transaction.id?.run(onNavigateToTransactionScreen)
+                    }
+                )
+            }
+        }
+    )
+
+    TimeSelectorCenterDialog(
+        state = selectorTimeDialogState,
+        initTime = remember(
+            monthSelectorState.year,
+            monthSelectorState.month
+        ) {
+            calendar.apply {
+                clear()
+                setYear(monthSelectorState.year)
+                setMonth(monthSelectorState.month)
+            }.time
+        },
+        type = TimeType.YEAR_MONTH,
+        onConfirm = {
+            val cal = calendar.apply { time = it }
+            monthSelectorState.onTimeChange(cal.getYear(), cal.getMonth())
         }
     )
 }
 
-@Preview
+@PreviewMultipleScreenSizes
 @Composable
 private fun PreviewAccountHome() = GooseTheme {
     val calendar = remember { Calendar.getInstance() }
