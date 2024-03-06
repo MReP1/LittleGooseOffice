96d4e8c0a2571c964d5a27fc0a04fabb9dc80e15
fne <2351267654@qq.com>
2024-03-07 00:44:28 +0800

    optimize rememberNotebookHomeStateHolder code

--- shared/feature/note/src/commonMain/kotlin/little.goose.note/ui/notebook/NotebookHomeStateHoler.kt
+++ shared/feature/note/src/commonMain/kotlin/little.goose.note/ui/notebook/NotebookHomeStateHoler.kt
@@ -4,34 +4,30 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import little.goose.data.note.bean.NoteWithContent
 import little.goose.data.note.domain.DeleteNoteAndItsBlocksListUseCase
 import little.goose.data.note.domain.DeleteNoteIdListFlowUseCase
 import little.goose.data.note.domain.GetNoteWithContentFlowUseCase
 import little.goose.shared.ui.architecture.MviHolder
-import little.goose.shared.ui.architecture.autoMutableStateFlowSaver
 import org.koin.compose.koinInject
 
 @Composable
-fun rememberNotebookHomeStateHolder(): MviHolder<NoteColumnState, NotebookHomeEvent, NotebookIntent> {
+fun rememberNotebookHomeStateHolder(
+    getNoteWithContentFlowUseCase: GetNoteWithContentFlowUseCase = koinInject(),
+    deleteNoteAndItsBlocksListUseCase: DeleteNoteAndItsBlocksListUseCase = koinInject(),
+    deleteNoteIdListFlowUseCase: DeleteNoteIdListFlowUseCase = koinInject()
+): MviHolder<NoteColumnState, NotebookHomeEvent, NotebookIntent> {
     val coroutineScope = rememberCoroutineScope()
-    val getNoteWithContentFlowUseCase: GetNoteWithContentFlowUseCase = koinInject()
-    val deleteNoteAndItsBlocksListUseCase: DeleteNoteAndItsBlocksListUseCase = koinInject()
-    val deleteNoteIdListFlowUseCase: DeleteNoteIdListFlowUseCase = koinInject()
 
-    val noteWithContents = remember(getNoteWithContentFlowUseCase) {
-        getNoteWithContentFlowUseCase().stateIn(coroutineScope, SharingStarted.Eagerly, emptyList())
+    val noteWithContents by produceState(emptyList<NoteWithContent>()) {
+        getNoteWithContentFlowUseCase().collect { value = it }
     }
 
     val event = remember { MutableSharedFlow<NotebookHomeEvent>() }
@@ -42,35 +38,22 @@ fun rememberNotebookHomeStateHolder(): MviHolder<NoteColumnState, NotebookHomeEv
         }
     }
 
-    val multiSelectedIds = rememberSaveable(saver = autoMutableStateFlowSaver()) {
-        MutableStateFlow<Set<Long>>(emptySet())
+    var multiSelectedIds by rememberSaveable {
+        mutableStateOf<Set<Long>>(emptySet())
     }
 
-    var noteColumnSavableState by rememberSaveable(stateSaver = NoteColumnState.saver) {
-        mutableStateOf(
-            NoteColumnState(
-                noteItemStateList = mapToNoteItemStateList(
-                    noteWithContents.value,
-                    multiSelectedIds.value
-                ),
-                isMultiSelecting = multiSelectedIds.value.isNotEmpty()
-            )
+    val noteColumnSavableState = rememberSaveable(
+        noteWithContents, multiSelectedIds,
+        saver = NoteColumnState.saver
+    ) {
+        NoteColumnState(
+            noteItemStateList = mapToNoteItemStateList(noteWithContents, multiSelectedIds),
+            isMultiSelecting = multiSelectedIds.isNotEmpty()
         )
     }
 
-    LaunchedEffect(noteWithContents, multiSelectedIds) {
-        combine(multiSelectedIds, noteWithContents) { multiSelectedNotes, noteWithContents ->
-            NoteColumnState(
-                noteItemStateList = mapToNoteItemStateList(noteWithContents, multiSelectedNotes),
-                isMultiSelecting = multiSelectedNotes.isNotEmpty()
-            )
-        }.collect {
-            noteColumnSavableState = it
-        }
-    }
-
     val cancelMultiSelecting = remember {
-        fun() { multiSelectedIds.value = emptySet() }
+        fun() { multiSelectedIds = emptySet() }
     }
 
     val action: (NotebookIntent) -> Unit = remember {
@@ -79,22 +62,19 @@ fun rememberNotebookHomeStateHolder(): MviHolder<NoteColumnState, NotebookHomeEv
                 NotebookIntent.CancelMultiSelecting -> cancelMultiSelecting()
 
                 NotebookIntent.SelectAllNotes -> {
-                    multiSelectedIds.value = noteWithContents.value
-                        .mapNotNull { it.note.id }.toSet()
+                    multiSelectedIds = noteWithContents.mapNotNull { it.note.id }.toSet()
                 }
 
                 is NotebookIntent.DeleteMultiSelectingNotes -> {
                     coroutineScope.launch {
-                        deleteNoteAndItsBlocksListUseCase(multiSelectedIds.value.toList())
+                        deleteNoteAndItsBlocksListUseCase(multiSelectedIds.toList())
                         cancelMultiSelecting()
                     }
                 }
 
                 is NotebookIntent.SelectNote -> {
-                    multiSelectedIds.update {
-                        it.toMutableSet().apply {
-                            if (intent.selected) add(intent.noteId) else remove(intent.noteId)
-                        }
+                    multiSelectedIds = multiSelectedIds.toMutableSet().apply {
+                        if (intent.selected) add(intent.noteId) else remove(intent.noteId)
                     }
                 }
             }
